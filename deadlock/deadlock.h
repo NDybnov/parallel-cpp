d02a9540f0a8617132ba3d724ece56c6f1c7cf67
NDybnov <nikita.dybnov@yandex.ru>
2021-07-11 21:20:24 +0200

    confused ungabunga

--- deadlock/deadlock.h
+++ deadlock/deadlock.h
@@ -1,20 +1,31 @@
 #pragma once
 
 
+#include <chrono>
+#include <thread>
+#include <mutex>
+
 class Deadlock {
  public:
-  Deadlock() {
-  }
+    Deadlock() {}
 
   void ThreadOne() {
-    // Your
+      m2_.lock();
+      std::this_thread::sleep_for(std::chrono::milliseconds(10));
+      m1_.lock();
+      m2_.unlock();
+      m1_.unlock();
   }
 
   void ThreadTwo() {
-    // Your
+      m1_.lock();
+      std::this_thread::sleep_for(std::chrono::milliseconds(10));
+      m2_.lock();
+      m2_.unlock();
+      m1_.unlock();
   }
 
  private:
-  // Your
+  std::mutex m1_;
+  std::mutex m2_;
 };
-
