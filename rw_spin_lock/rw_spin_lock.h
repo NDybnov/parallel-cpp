4dde488836b7f77fc745cae60b5c57124ad2f080
NDybnov <nikita.dybnov@yandex.ru>
2021-07-12 07:10:46 +0200

    rw_spin_lock

--- rw_spin_lock/rw_spin_lock.h
+++ rw_spin_lock/rw_spin_lock.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <atomic>
+#include <thread>
+
 
 class RWSpinLock {
  public:
@@ -7,22 +10,36 @@ class RWSpinLock {
   }
 
   void LockRead() {
-    // Your code
+    while (counter_and_flag_.load() & 1) {
+        std::this_thread::yield();
+    }
+    counter_and_flag_.fetch_add(2);
   }
 
   void UnlockRead() {
-    // Your code
+      counter_and_flag_.fetch_sub(2);
   }
 
   void LockWrite() {
-    // Your code
+    for (;;) {
+        counter_and_flag_.fetch_or(1);
+        while (counter_and_flag_.load() != 1) {
+            std::this_thread::yield();
+            counter_and_flag_.fetch_or(1);
+        }
+        unsigned int CAS_expected = 1;  // чтобы менялось
+        if (counter_and_flag_.compare_exchange_weak(CAS_expected, 3)) {
+            break;
+        } else {
+            std::this_thread::yield();
+        }
+    }
   }
 
   void UnlockWrite() {
-    // Your code
+    counter_and_flag_.store(0);
   }
 
  private:
-  // Your code
+    std::atomic<unsigned int> counter_and_flag_ = 0;
 };
-
