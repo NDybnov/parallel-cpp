df953ce25428439ad95235e6118580b600a445d7
NDybnov <nikita.dybnov@yandex.ru>
2021-07-12 05:44:06 +0200

    hello_world

--- hello_world/hello_world.h
+++ hello_world/hello_world.h
@@ -1,23 +1,35 @@
 #pragma once
 
 #include <ostream>
+#include <thread>
+#include <mutex>
+#include <vector>
 #include <string_view>
-
+#include <iostream>
 
 class HelloWorld {
  public:
   static constexpr std::string_view kHelloPrefix = "Hello, World! From thread ";
 
-  HelloWorld(size_t /*n_threads*/)
-  {
-    // Your code
-  }
+  HelloWorld(size_t n_threads)
+    :n_threads_(n_threads)
+  {}
 
-  void SayHello(std::ostream& /*os*/) {
-    // Your code
+  void SayHello(std::ostream& os) {
+    std::mutex os_mutex;
+    std::vector<std::thread> threads;
+    for (size_t i = 0; i < n_threads_; ++i) {
+        threads.emplace_back([&] () {
+            os_mutex.lock();
+            os << kHelloPrefix <<  std::this_thread::get_id() << '\n';
+            os_mutex.unlock();
+        });
+    }
+    for (size_t i = 0; i < n_threads_; ++i) {
+        threads[i].join();
+    }
   }
 
  private:
-  // Your code
+  const size_t n_threads_;
 };
-
