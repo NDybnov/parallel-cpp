4d7083163b8538f7ceb5bbc59604ac4d05624c4c
NDybnov <nikita.dybnov@yandex.ru>
2021-07-27 18:00:08 +0200

    hello_world

--- hello_world/hello_world.h
+++ hello_world/hello_world.h
@@ -2,22 +2,29 @@
 
 #include <ostream>
 #include <string_view>
+#include <thread>
 
 
 class HelloWorld {
  public:
   static constexpr std::string_view kHelloPrefix = "Hello, World! From thread ";
 
-  HelloWorld(size_t /*n_threads*/)
+  HelloWorld(size_t n_threads)
+    :n_threads_(n_threads)
   {
-    // Your code
   }
 
-  void SayHello(std::ostream& /*os*/) {
-    // Your code
+  void SayHello(std::ostream& os) {
+    std::vector<std::thread> threads;
+    for (size_t i = 0; i < n_threads_; ++i) {
+      threads.emplace_back([&] () {os << kHelloPrefix << std::this_thread::get_id() << '\n';}
+    }
+    for (size_t i = 0; i < n_threads_; ++i) {
+      threads[i].join();
+    }
   }
 
  private:
-  // Your code
+  const size_t n_threads_;
 };
 
