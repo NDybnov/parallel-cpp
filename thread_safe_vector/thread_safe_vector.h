b95d6dfd401d9b19089aab3d40456d323b1b2d61
NDybnov <nikita.dybnov@yandex.ru>
2021-07-28 14:53:15 +0200

    thread_safe_vector2

--- thread_safe_vector/thread_safe_vector.h
+++ thread_safe_vector/thread_safe_vector.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <atomic>
+#include <mutex>
+#include <thread>
+#include <shared_mutex>
 #include <vector>
 
 
@@ -7,25 +11,43 @@ template <typename T>
 class ThreadSafeVector {
  public:
   ThreadSafeVector() {
+      realloc_.store(false);
   }
 
   T operator[](size_t index) const {
-    // Your code
-    return vector_[index];
+    vector_reader_mutex_.lock_shared();
+    while (realloc_.load()) {
+        vector_reader_mutex_.unlock_shared();
+        std::this_thread::yield();
+        vector_reader_mutex_.lock_shared();
+    }
+    T ans = vector_[index];
+    vector_reader_mutex_.unlock_shared();
+    return ans;
   }
 
   size_t Size() const {
-    // Your code
     return vector_.size();
   }
 
   void PushBack(const T& value) {
-    // Your code
-    vector_.push_back(value);
+    vector_writer_mutex_.lock();
+    if (vector_.capacity() == vector_.size()) {
+        realloc_.store(true);
+        vector_reader_mutex_.lock();
+        vector_.push_back(value);
+        vector_reader_mutex_.unlock();
+        realloc_.store(false);
+    } else {
+        vector_.push_back(value);
+    }
+    vector_writer_mutex_.unlock();
   }
 
  private:
-  // Your code
+  mutable std::shared_mutex vector_reader_mutex_;
+  mutable std::mutex vector_writer_mutex_;
+  mutable std::atomic_bool realloc_;
   std::vector<T> vector_;
 };
 
