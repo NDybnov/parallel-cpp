e8db49ae9c218df9ee6bb597e18e4d0101ecf50b
NDybnov <nikita.dybnov@yandex.ru>
2021-07-28 14:57:09 +0200

    unbuffered_channel

--- unbuffered_channel/unbuffered_channel.h
+++ unbuffered_channel/unbuffered_channel.h
@@ -1,28 +1,62 @@
 #pragma once
 
+#include <condition_variable>
+#include <exception>
+#include <mutex>
 #include <optional>
-
 
 template <typename T>
 class UnbufferedChannel{
  public:
-  UnbufferedChannel() {
-  }
+  UnbufferedChannel() {}
 
-  void Send(const T& /*value*/) {
-    // Your code
+  void Send(const T& value) {
+      std::unique_lock<std::mutex> s_lock(sblock__);
+      std::unique_lock<std::mutex> locker(all_block_);
+      if (closed_) {
+        throw std::runtime_error("Channel is closed");
+      }
+      placed_ = true;
+      value_pointer_ = &value;
+      ready_to_ship_.notify_one();
+      grab_.wait(locker, [&] {
+        return !placed_ || closed_;
+      });
+      if (placed_) {
+        throw std::runtime_error("Channel is closed");
+      }
   }
 
   std::optional<T> Recv() {
-    // Your code
-    return std::nullopt;
+      std::unique_lock<std::mutex> r_lock(rblock_);
+      std::unique_lock<std::mutex> locker(all_block_);
+      ready_to_ship_.wait(locker, [&] {
+        return placed_ || closed_;
+      });
+      if (closed_) {
+        return std::nullopt;
+      }
+      placed_ = false;
+      std::optional<T> ret_opt(*value_pointer_);
+      grab_.notify_one();
+      return ret_opt;
   }
 
   void Close() {
-    // Your code
+    std::unique_lock<std::mutex> locker(all_block_);
+    closed_ = true;
+    ready_to_ship_.notify_all();
+    grab_.notify_all();
   }
 
  private:
-  // Your code
-};
+  T const *value_pointer_;
+  bool closed_ = false;
+  bool placed_ = false;
+  std::mutex all_block_;
+  std::mutex sblock__;
+  std::mutex rblock_;
+  std::condition_variable ready_to_ship_;
+  std::condition_variable grab_;
 
+};
