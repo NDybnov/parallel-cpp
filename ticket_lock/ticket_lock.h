2b8c28663cdf7e94191839fb77671ff966463b32
NDybnov <nikita.dybnov@yandex.ru>
2021-07-31 14:06:41 +0200

    ticket_lock2

--- ticket_lock/ticket_lock.h
+++ ticket_lock/ticket_lock.h
@@ -8,7 +8,7 @@ class TicketLock {
   TicketLock() {}
 
   void Lock() {
-    std::atomic<int> cur = next_ticket_.fetch_add(1);
+    uint64_t cur = next_ticket_.fetch_add(1);
       while (now_serving_ != cur) {
         std::this_thread::yield();   
       }
