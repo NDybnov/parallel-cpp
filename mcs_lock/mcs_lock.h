6038ae20a8842b646d7d5e221d510f9f137c08f7
NDybnov <nikita.dybnov@yandex.ru>
2021-07-31 15:38:01 +0200

    mcs_lock2

--- mcs_lock/mcs_lock.h
+++ mcs_lock/mcs_lock.h
@@ -1,20 +1,47 @@
 #pragma once
 
+#include <atomic>
+#include <thread>
+
+#include <iostream>
+
+class Node {
+ public:
+
+  Node() {}
+
+  std::atomic<bool> acquired{true};
+  std::atomic<Node*> next{nullptr};
+
+};
 
 class MCSLock {
  public:
-  MCSLock() {
-  }
+  MCSLock() {}
 
   void Lock() {
-    // Your code
+    node_.next.store(nullptr);
+    node_.acquired.store(true);
+    Node *prev_node_ = tail_.exchange(&node_);
+    if (prev_node_) {
+      node_.acquired.store(false);
+      prev_node_->next.store(&node_);
+    }
+    while (!node_.acquired.load()) {
+      std::this_thread::yield();
+    }
   }
 
   void Unlock() {
-    // Your code
+    Node* node_ptr = &node_;
+    if (tail_.compare_exchange_weak(node_ptr, nullptr)) {
+      return;
+    }
+    while (!node_.next.load());
+    node_.next.load()->acquired.store(true);
   }
 
  private:
-  // Your code
+  std::atomic<Node*> tail_{nullptr};
+  thread_local static inline Node node_ = Node();
 };
-
